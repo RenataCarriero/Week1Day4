2a1b2cde052162553cf718bab3643b65565e9158
Renata Carriero <Renata.Carriero@icubed.it>
2021-07-15 10:30:58 +0200

    Aggiunta traccia dell' Esercizio1

---
+++ Week1Day4.Esercizio1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Week1Day4.Esercizio1
+{
+    class Program
+    {
+        /*Gioco del lancio dei Dadi
+
+        Si chiede all'utente quanti dadi vuole lanciare.
+        L'utente inserisce un numero per provare ad indovinare 
+        la somma dei valori usciti dal lancio dei dadi.
+
+        Il computer lancia i dadi, quindi sorteggia i numeri (random tra 1 e 6).
+
+        Se la somma dei due numeri random è pari al numero scelto dall'utente, l'utente vince
+        Se l'utente vince, stampare 'hai vinto', altrimenti 'hai perso'.
+
+        Finita la partita, l'utente deve poter rigiocare.
+
+        Requisiti:
+        Verificare che l'utente inserisca un intero e che sia compreso tra i valori possibili/accettabili.
+        Se la verifica non va a buon fine, l'utente deve potere inserire nuovamente qualcosa. 
+
+        Opzionale-> Creare un metodo per l'inserimento dei numeri e la verifica, 
+        uno per il controllo della vittoria (da chiamare nel main).*/
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
