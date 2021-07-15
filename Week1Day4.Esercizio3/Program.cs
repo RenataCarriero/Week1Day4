d6c55e237a13b7f3a9e23066f3912c8558bc58be
Renata Carriero <Renata.Carriero@icubed.it>
2021-07-15 16:59:58 +0200

    Aggiunta Soluzione Indovina Numero. Aggiunti Esercizi su Stringhe e Lista della Spesa.

--- Week1Day4.Esercizio3/Program.cs
+++ Week1Day4.Esercizio3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Week1Day4.Esercizio3
 {
@@ -6,7 +7,111 @@ namespace Week1Day4.Esercizio3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool continua = true;
+            while (continua)
+            {
+                Console.WriteLine("\n-----------------------------------");
+                Console.WriteLine("Benvenuto!");
+                Console.WriteLine("-----------------------------------\n");
+                Console.Write("Inserisci il tuo nome: --> ");
+                string nome = Console.ReadLine();
+                Console.WriteLine($"Ciao {nome}!");
+
+                do
+                {
+                    char scelta = SceltaMenu();
+                    if (scelta == 's')
+                    {
+                        SchermoNuovaPartita();
+                    }
+                    else
+                    {
+                        SchermoUscita();
+                        continua = false;
+                    }
+                } while (continua == true);
+            }
+        }
+
+        public static void SchermoUscita()
+        {
+            Console.WriteLine("\nArrivederci");
+            Console.WriteLine("Torna presto a giocare!\n\n");
+        }
+
+        public static void SchermoNuovaPartita()
+        {
+            int numeroSegreto = new Random().Next(1, 101);
+            
+            //Scrittura su File
+            StreamWriter sw = new StreamWriter(@"C:\Users\RenataCarriero\source\repos\Week1Day4\Week1Day4.Esercizio3\NumeroDaIndovinare.txt");
+            sw.WriteLine("Numero da indovinare: " + numeroSegreto);
+            sw.Close();
+            //Fine scrittura su File
+
+            Console.WriteLine("\nHo scelto un numero tra 1 e 100. Prova a indovinarlo.");
+            bool trovato = false;
+            int numeroDiTentativi = 0;
+            int tentativo;
+            string suggerimento=null;
+
+            do
+            {
+                Console.WriteLine($"Finora hai effettuato {numeroDiTentativi} tentativi.");
+                numeroDiTentativi++;
+                Console.Write($"Fai il {numeroDiTentativi}° tentativo (0 se vuoi interrompere il gioco): ");
+                while (!int.TryParse(Console.ReadLine(), out tentativo))
+                {
+                    Console.WriteLine("Devi inserire un numero. Riprova.");
+                    Console.Write($"Fai il {numeroDiTentativi}° tentativo (0 se vuoi interrompere il gioco): ");
+                }
+                while ((tentativo < 0) || (tentativo > 100))
+                {
+                    Console.WriteLine("Il valore deve essere compreso tra 1 e 100. (0 se vuoi interrompere il gioco)");
+                    Console.Write($"Fai il {numeroDiTentativi}° tentativo (0 se vuoi interrompere il gioco): ");
+                    tentativo = int.Parse(Console.ReadLine()); //TODO mancano i controlli.
+                }
+
+                if (tentativo == 0)
+                {
+                    Console.WriteLine("\nPartita Interrotta");
+                    Console.WriteLine("Il numero da indovinare era: " + numeroSegreto);
+                    return;
+                }
+                else if (tentativo == numeroSegreto)
+                {
+                    trovato = true;
+                    Console.WriteLine("\n****** Bravo, hai indovinato! ******");
+                    Console.WriteLine("\nHai effettuato in totale " + numeroDiTentativi + " tentativi");
+                    return;
+                }
+                else
+                {
+                    if (tentativo < numeroSegreto)
+                    {
+                        suggerimento = "Prova con un numero piu' alto";
+                    }
+                    else if (tentativo > numeroSegreto)
+                    {
+                        suggerimento = "Prova con un numero piu' basso";
+                    }
+                    Console.WriteLine("Suggerimento: " + suggerimento);
+                }
+            } while (trovato == false);
+        }
+
+        public static char SceltaMenu()
+        {
+            Console.WriteLine("\n-----------Menu---------- ");
+            Console.WriteLine("\nVuoi fare una nuova partita? s/n");
+
+            char risposta = Console.ReadKey().KeyChar;
+            while ((risposta != 's') && (risposta != 'n'))
+            {
+                Console.WriteLine("Errore: devi scegliere s/n. Vuoi fare una nuova partita?");
+                risposta = char.Parse(Console.ReadLine());
+            }
+            return risposta;
         }
     }
 }
---
+++ Week1Day4.Esercizio5/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Week1Day4.Esercizio5
+{
+    class Program
+    {
+        //Scrivere un programma che permetta all'utente di creare la sua lista della spesa.
+
+        //Si chiede all'utente quanti prodotti vuole inserire nell'elenco. (numero intero valido e positivo).
+        //Quindi si chiede all'utete di inserire i prodotti (esempio "uova", "pasta"..).
+        //Non si possono inserire 2 prodotti uguali.
+        //(Opzionale: "uova", "UOVA", "Uova" sono da intendersi uguali, quindi no-case-sensitive)
+
+        //Se l'utente inserisce un prodotto già presente, gli si mostra un messaggio del tipo "prodotto già inserito".
+        //Completato l'elenco della spesa, stampare un riepilogo con tutti i prodotti che ha inserito l'utente.
+
+        //Requisiti: utilizzare un array (non una lista). Utilizzare le routine (es. ScriviListaSpesa e StampaListaSpesa)
+
+        //Opzionale. Stampare la lista della spesa su un file "listaSpesa.txt" invece che a video.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quanti prodotti vuoi inserire nella tua lista della spesa?");
+            int n = 0;
+            while (!int.TryParse(Console.ReadLine(), out n) || n < 0)
+            {
+                Console.WriteLine("Inserisci un numero positivo. Riprova.");
+            }
+            string[] listaSpesa = ScriviListaDellaSpesa(n);
+
+            StampaListaSpesa(listaSpesa);
+        }
+
+        private static void StampaListaSpesa(string[] listaSpesa)
+        {
+            Console.WriteLine("Ecco il riepilogo della tua lista della spesa");
+            for (int i = 0; i < listaSpesa.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {listaSpesa[i]}");
+            }
+        }
+
+        private static string[] ScriviListaDellaSpesa(int n)
+        {
+            string[] products = new string[n];
+
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine($"Inserisci il {i + 1}° prodotto");
+                string product = Console.ReadLine().ToUpper();
+
+                int found = Array.IndexOf(products, product);
+
+                if (found > -1) //se c'è già il prodotto
+                {
+                    Console.WriteLine($"Hai già inserito {product}!");
+                    i--; //torno indietro e scrivo un altro prodotto
+                }
+                else
+                {
+                    products[i] = product; //lo assegna all'array 
+                }
+            }
+            return products;
+        }
+    }
+}
